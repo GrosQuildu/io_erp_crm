fbb85848c8e0e1bb3d7cefe4858b28609d0be1f0
Marcin <mblacharski>
2018-01-11 14:02:07 +0100

    aaaa

--- front/front2/src/main/java/erp/backend/api/erp/ArticlesControllerApi.java
+++ front/front2/src/main/java/erp/backend/api/erp/ArticlesControllerApi.java
@@ -1,55 +1,31 @@
 package main.java.erp.backend.api.erp;
 
-import main.java.erp.backend.api.common.ServerConfiguration;
+import com.google.gson.Gson;
+import main.java.erp.backend.api.ConnectionApi;
 import main.java.erp.backend.model.erp.Article;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
+import main.java.erp.frontend.login.Login;
+import org.apache.http.entity.StringEntity;
 
-import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class ArticlesControllerApi {
-    //RestTemplate restTemplate = new RestTemplate();
-    ResourceOwnerPasswordResourceDetails resourceDetails = new ResourceOwnerPasswordResourceDetails();
-    private HttpHeaders httpHeaders = new HttpHeaders();
+    private ConnectionApi connection = Login.connection;
+    private Gson gson = new Gson();
 
-    OAuth2RestTemplate restTemplate;
-    private String baseUrl = ServerConfiguration.address + ":" + ServerConfiguration.port + "/" + ServerConfiguration.baseUrl + "/erp/articles";
-
-    public ArticlesControllerApi(){
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        headers.add("accept", "application/json");
-        resourceDetails.setAccessTokenUri("http://javafx-client:R3GMPX2DQHD1234VFG929ABW@localhost:8080/api/oauth/token");
-        resourceDetails.setClientId("javafx-client");
-        resourceDetails.setClientSecret("R3GMPX2DQHD1234VFG929ABW");
-        resourceDetails.setGrantType("password");
-        resourceDetails.setUsername("admin@io_erp_crm.com");
-        resourceDetails.setPassword("O9HKN2NJZ20T");
-        String parameters = "grant_type=password&username=admin@io_erp_crm.com&password=O9HKN2NJZ20T";
-        HttpEntity<String> request = new HttpEntity<>(parameters, headers);
-        restTemplate = new OAuth2RestTemplate(resourceDetails, new DefaultOAuth2ClientContext());
-        HttpEntity<String> token = restTemplate.postForEntity(resourceDetails.getAccessTokenUri(), request, String.class);
-    }
 
     public List<Article> getArticles(){
         List<Article> articles = new ArrayList<>();
         articles.addAll(
-                Arrays.asList(restTemplate.getForEntity(baseUrl, Article[].class, Collections.emptyMap()).getBody())
+                Arrays.asList( gson.fromJson(connection.getObjects(ConnectionApi.ObjectType.ARTICLES), Article[].class) )
         );
         return articles;
+    }
+
+    public Integer createArticle(Article article){
+        System.out.println(article.serialize());
+        return connection.createObject(article.serialize(), ConnectionApi.ObjectType.ARTICLES);
     }
 }

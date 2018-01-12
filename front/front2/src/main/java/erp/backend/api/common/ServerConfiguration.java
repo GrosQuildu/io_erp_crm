1ae4643c85a2c2ccf083f34b9bc71356e9593e20
Marcin <mblacharski>
2018-01-12 05:36:43 +0100

    ..

--- front/front2/src/main/java/erp/backend/api/common/ServerConfiguration.java
+++
@@ -1,7 +0,0 @@
-package main.java.erp.backend.api.common;
-
-public class ServerConfiguration {
-    public static String address = "http://localhost";
-    public static String port = "8080";
-    public static String baseUrl = "api";
-}
